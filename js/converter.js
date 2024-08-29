4cc0434a8d36ba21954191329a599aadf091cb45
Melissa Yamileth Cruz Pneda <ia.vaspin@ufg.edu.sv>
2024-08-29 07:56:00 -0600

    Finalizacion de codigo HTML, Finalizacion de hoja de estilos, Inicio de logica del negocio

---
+++ js/converter.js
@@ -0,0 +1,8 @@
+const inputField = document.getElementById("input-temp");
+const fromUnitField = document.getElementById("input-unit");
+const toUnitField= document.getElementById("output-unit");
+const outputField = document.getElementById("output-temp");
+const form = document.getElementById("converter");
+
+
+console.log("Melissa Yamileth Cruz Pineda");
